c434ee179e6cc494d685a70e563f412891d8d2f8
Guillermo Cuesta <alumno06git@gmail.com>
2025-05-09 16:46:15 +0000

    dim_users hecho

--- models/marts/intermediate/int_order_line_allocations.sql
+++ models/marts/intermediate/int_order_line_allocations.sql
@@ -2,14 +2,11 @@
 
 with
     order_items as (
-
         select order_id, product_id, quantity
         from {{ ref("stg_sql_server_dbo__order_items") }}
-
     ),
 
     orders as (
-
         select
             order_id,
             address_id,
@@ -25,21 +22,21 @@ with
             order_cost,
             order_total
         from {{ ref("stg_sql_server_dbo__orders") }}
-
     ),
 
     products as (
-
-        select product_id, name as product_name, price, inventory, peso as peso_item
+        select 
+            product_id, 
+            name as product_name, 
+            price, 
+            inventory, 
+            peso 
         from {{ ref("stg_sql_server_dbo__products") }}
-
     ),
 
     joined_data as (
-
         select
-            {{ dbt_utils.generate_surrogate_key(["oi.order_id", "oi.product_id"]) }}
-            as order_item_id,
+            {{ dbt_utils.generate_surrogate_key(["oi.order_id", "oi.product_id"]) }} as order_item_id,
 
             -- Fact fields
             oi.order_id,
@@ -51,6 +48,7 @@ with
             o.promo_id,
             o.user_id,
             o.shipping_service,
+            o.shipping_cost,
             o.created_at,
             o.estimated_delivery_at,
             o.delivered_at,
@@ -63,20 +61,20 @@ with
             p.product_name,
             p.price,
             p.inventory,
-            p.peso_item,
+            p.peso,
 
             -- Reparto shipping_cost
-            p.peso_item * oi.quantity as peso_total,
-            sum(peso_total) over (partition by oi.order_id) as peso_total_pedido,
+            p.peso * oi.quantity as peso_item,
+            sum(peso_item) over (partition by oi.order_id) as peso_total_pedido,
             round(
-                (peso_total)
-                / nullif(sum(peso_total) over (partition by oi.order_id), 0)
-                * shipping_cost,
+                (peso_item)
+                / nullif(sum(peso_item) over (partition by oi.order_id), 0)
+                * o.shipping_cost,
                 2
             ) as shipping_cost_proporcional,
 
             -- Reparto descuento
-            round((o.order_cost + shipping_cost) - o.order_total, 2) as promo_discount,
+            round((o.order_cost + o.shipping_cost) - o.order_total, 2) as promo_discount,
             round(
                 oi.quantity
                 / nullif(sum(oi.quantity) over (partition by oi.order_id), 0)
@@ -87,7 +85,6 @@ with
         from order_items oi
         left join orders o on oi.order_id = o.order_id
         left join products p on oi.product_id = p.product_id
-
     )
 
 select *
